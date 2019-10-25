2c747a53f079154c3e1f8cbfa2f96f2a1e3216b6
philipd <philipd@stud.ntnu.no>
2019-10-25 16:12:49 +0200

    #6 implemented redux, with thunk for API calls

---
+++ src/redux/actions/countActions.js
@@ -0,0 +1,7 @@
+// example actions
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+
+// example action creators
+export const increment = () => ({ type: INCREMENT });
+export const decrement = () => ({ type: DECREMENT });
