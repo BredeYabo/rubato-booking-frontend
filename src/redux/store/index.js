8a1cec5c8f154718a8751e9ff28951cc426c8a25
Philip Dahlstrøm <philipd@stud.ntnu.no>
2019-10-25 16:37:33 +0200

    redux-fix (#8)

---
+++ src/redux/store/index.js
@@ -0,0 +1,9 @@
+import rootReducer from '../reducers/rootReducer'
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+
+// thunk enables redux to handle API requests
+export const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+);
