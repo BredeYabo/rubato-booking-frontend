6a855e485774b8e96114cb2139fd54731e462a85
sverre <sverrefu@ntnu.no>
2019-11-08 16:39:28 +0100

    Started on implementing the email funcionality

--- src/components/ContactComponent/index.js
+++ src/components/ContactComponent/index.js
@@ -1,22 +1,60 @@
 import React from "react";
 import "./styles/index.css"
+import transport from "nodemailer"
+import creds from "../../config";
 
 
 export default class ContactComponent extends React.Component {
 
-    sendEmail = (event, name, email, subject, message) => {
+    sendEmail = (e) => {
+        e.preventDefault();
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const subject = document.getElementById("subject").value;
+        const message = document.getElementById("contact-message").value;
 
-        /*
         const htmlBody = `
-            <p>You have a new contact request</p>
-            <h3>Contact information:</h3>
-            <ul>
-               <li>Name: ${form.name} </li>
-            </ul>
+            <div>
+                <p>You have a new contact request</p>
+                <h3>Contact information:</h3>
+                <ul>
+                   <li>Name: ${name} </li>
+                   <li>From: ${email}</li>
+                   <li>Subject: ${subject}</li>
+                </ul>
+                <h3>Message from sender:</h3>
+                <p>${message}</p>
+            </div>
         `;
 
-      const transporter = transport.createTransport()
-        */
+        const transporter = transport.createTransport({
+            host: "mail.one.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: creds.USER,
+                pass: creds.PASS
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
+
+        const mail = {
+            from: creds.USER,
+            to: "sverre@rubato.no",
+            subject: subject,
+            html: htmlBody
+        };
+
+        transporter.sendMail(mail, (err, data) => {
+            if (err) {
+                console.log("Failed to send...\n" + err)
+            } else {
+                console.log("Sent it...")
+            }
+        })
+
 
     };
 
@@ -27,24 +65,24 @@ export default class ContactComponent extends React.Component {
                 <div id="contact-image">
                     <img src="" alt="Kontakt bilde"/>
                 </div>
-                <form id="contact-form" metod="POST" action="">
+                <form id="contact-form" metod="POST" onSubmit={this.sendEmail.bind(this)} >
                     <div id="div_input">
                         <label htmlFor="name-input">Name</label><br/>
-                        <input type="text" className="standard-input" name="name" required/>
+                        <input type="text" className="standard-input" id="name" required/>
                     </div>
                     <div id="div_email">
                         <label htmlFor="email-input">E-post</label><br/>
-                        <input type="email" className="standard-input" name="email" required/>
+                        <input type="email" className="standard-input" id="email" required/>
                     </div>
                     <div id="div_subject">
                         <label htmlFor="subject_input">Subject</label><br/>
-                        <input type="text" className="standard-input" name="subject" required/>
+                        <input type="text" className="standard-input" id="subject" required/>
                     </div>
                     <div id="div_message">
                         <label htmlFor="message_input">Message</label><br/>
-                        <textarea type="text" id="contact-message" rows="15" cols="46" name="message" />
+                        <textarea type="text" id="contact-message" rows="15" cols="46"  />
                     </div>
-                    <button type="submit" className="nav-button" id="button_login" onClick={(e) => this.sendEmail()}>Send</button>
+                    <button type="submit" className="nav-button" id="button_login">Send</button>
                 </form>
             </div>
         )
