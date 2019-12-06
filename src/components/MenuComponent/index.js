2e162f796615c87e731b2cd6b5ba249021f8d64d
philipd <philipd@stud.ntnu.no>
2019-12-06 13:44:15 +0100

    replaced react-burgermenu with react-modal, some other minor styling

--- src/components/MenuComponent/index.js
+++ src/components/MenuComponent/index.js
@@ -1,16 +1,16 @@
 import React,{Component} from 'react';
 import styles from './styles/styles.module.css';
 import '../../res/fonts/futura.ttf';
-import crossIcon from '../../res/icons/cross.svg';
-import burgerIcon from '../../res/icons/burgermenu.svg'
+import {ReactComponent as CrossIcon} from '../../res/icons/cross.svg';
+import {ReactComponent as BurgerIcon} from '../../res/icons/burgermenu.svg'
 import { Link } from "react-router-dom";
 import {connect} from "react-redux";
-import {slide as BurgerMenu} from 'react-burger-menu';
-import {decorator as reduxBurgerMenu} from 'redux-burger-menu';
 import windowSize from 'react-window-size';
 import cx from 'classnames';
+import Modal from 'react-modal';
 
-// import ExampleComponent from "./components/ExampleComponent";
+Modal.setAppElement('#root');
+
 class MenuComponent extends Component{
 
     constructor(props) {
@@ -20,17 +20,17 @@ class MenuComponent extends Component{
         }
     }
 
-    handleStateChange = (state) =>{
-        this.setState({menuOpen: state.isOpen})
+    openMenu = () => {
+        this.setState({menuOpen:true});
     };
+
 
     closeMenu = () => {
-        console.log(this.state.menuOpen);
-        this.setState({menuOpen:false})
+        this.setState({menuOpen:false});
     };
 
-    logoutBurger = () =>{
-        this.props.logout();
+    logInOutBurger = () =>{
+        // logic can be added here if needed
         this.closeMenu()
     };
 
@@ -38,28 +38,39 @@ class MenuComponent extends Component{
         return(
             <div className={styles.menu_list}>
                 {this.props.windowWidth < 780 ?
-                    <BurgerMenu menuClassName={styles.burgerMenu}
-                                burgerButtonClassName={this.state.menuOpen? styles.burgerIconHidden: styles.burgerIconNormal}
-                                crossButtonClassName={styles.burgerCrossIcon}
-                                itemListClassName={styles.burgerItem}
-                                right
-                                noOverlay
-                                disableAutoFocus
-                                disableOverlayClick
-                                isOpen={this.state.menuOpen}
-                                width={this.props.windowWidth}
-                                onStateChange = {(state) => this.handleStateChange(state)}
-                                customCrossIcon={ <img src={crossIcon} alt={'X'}/>}
-                                customBurgerIcon={<img src={burgerIcon} alt={'='}/>}
-                    >
-                        { this.props.username ?
-                            <Link to="/" className={cx(styles.nav_button, styles.is_logged_in)} onClick={() => this.logoutBurger()} >logg ut</Link>
-                            :
-                            <Link to="/login" className={ cx(styles.nav_button_burger,styles.is_not_logged_in)} onClick={this.closeMenu} >logg inn</Link>
-                        }
-                        <Link to="/kontakt" className={styles.nav_button_burger} onClick={this.closeMenu} >kontakt</Link>
-                        <Link to="/om" className={styles.nav_button_burger} onClick={this.closeMenu}>om</Link>
-                    </BurgerMenu>
+                    <div>
+                        <BurgerIcon className={this.state.menuOpen ? styles.burgerIconHidden : styles.burgerIconNormal}
+                                    onClick={this.openMenu}/>
+                        <Modal
+                            isOpen={this.state.menuOpen}
+                            onRequestClose={this.closeMenu}
+                            className={styles.modalMenu}
+                            contentLabel={"test"}
+                        >
+                            <div>
+                                <CrossIcon className={styles.crossIcon} onClick={this.closeMenu}/>
+                                <div className={styles.menu_button_wrapper}>
+                                    {
+                                        this.props.username ?
+                                            <Link to="/" className={cx(styles.nav_button, styles.is_logged_in)}
+                                                  onClick={() => this.logInOutBurger}>logg ut</Link>
+                                            :
+                                            <Link to="/login" className={cx(styles.nav_button_burger, styles.is_not_logged_in)}
+                                                  onClick={this.logInOutBurger}>logg inn</Link>
+                                    }
+                                    <Link to="/kontakt" className={styles.nav_button_burger} onClick={this.closeMenu} >kontakt</Link>
+                                    <Link to="/om" className={styles.nav_button_burger} onClick={this.closeMenu}>om</Link>
+                                    <a href="https://rubato.no/"
+                                       className={styles.nav_button_burger}
+                                       onClick={this.closeMenu}
+                                       target="_blank" rel="noopener noreferrer"
+                                    >
+                                        blogg
+                                    </a>
+                                </div>
+                            </div>
+                        </Modal>
+                    </div>
                     :
                     <div className={styles.menu_list}>
                         { this.props.username ?
@@ -69,6 +80,13 @@ class MenuComponent extends Component{
                         }
                         <Link to="/kontakt" className={styles.nav_button}>kontakt</Link>
                         <Link to="/om" className={styles.nav_button}>om</Link>
+                        <a href="https://rubato.no/"
+                           className={styles.nav_button}
+                           onClick={this.closeMenu}
+                           target="_blank" rel="noopener noreferrer"
+                        >
+                            blogg
+                        </a>
                     </div>
                 }
             </div>
@@ -86,4 +104,4 @@ const mapDispatchToProps = {
 
 };
 
-export default connect(mapStateToProps, mapDispatchToProps) (windowSize(reduxBurgerMenu(MenuComponent)));
+export default connect(mapStateToProps, mapDispatchToProps) (windowSize(MenuComponent));
